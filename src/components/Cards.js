6f9867fb5baa470ea2e7ed036c4828e6ce17eb82
Shweta Kale <shwetakale144@gmail.com>
2021-04-15 10:56:28 +0530

    This filter works on same url

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -62,23 +62,36 @@ const useStyles = makeStyles(theme => ({
         backgroundColor:'white',
     },
     activeYear:{
+        backgroundColor:'#cfd',
+        color:'blue',
+        fontSize:'28px'
+    },
+    launchSuccess:{
         backgroundColor:'yellow'
     }
   }));
   
 const years = [2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020];
+const launch_success = ['true','false'];
 const Cards=()=>{
     const classes = useStyles();
     const [isLoading, setIsLoading] = useState(false);
     const [cardData, setCardData] = useState();
     const [activeYear, setActiveYear] = useState(null);
-    const [launchSuccess, setLaunchSuccess] = useState('');
+    const [launchSuccess, setLaunchSuccess] = useState(null);
     const handleYear= (e,yr)=>{
         if(activeYear==yr)
             setActiveYear(null);
         else
             setActiveYear(yr);
     };
+
+    const handleLaunch=(e,launch)=>{
+        if(launchSuccess==launch)
+            setLaunchSuccess(null);
+        else
+            setLaunchSuccess(launch);
+    }
     useEffect(()=>{
 
         setIsLoading(true);
@@ -96,7 +109,7 @@ const Cards=()=>{
         <div>
                 {console.log("dttt ** ",cardData)}
             {cardData?(
-        <Grid  className={classes.root} container justify="center" display="flex">
+            <Grid  className={classes.root} container justify="center" display="flex">
                 <Grid item xs={2}>
                     <div className={classes.filter}>
                         Filters
@@ -111,39 +124,52 @@ const Cards=()=>{
                                 className={activeYear==years[index]?classes.activeYear:''}
                             />
                         </Grid>
-                        )
-                       
+                        )  
                     )}
+                    </Grid>
+
+                    <h2>Launch Success</h2>
+                    <Grid container display="flex" style={{padding:'8px'}}>
+                        {launch_success.map((launch,index)=>(
+                            <Grid item xs={12} sm={4} lg={6} style={{padding:'8px'}}>
+                                <Chip
+                                    key={index}
+                                    label={launch}
+                                    onClick={e=>handleLaunch(e,launch)}
+                                    className={launchSuccess==launch_success[index]?classes.launchSuccess:''}
+                                />
+                            </Grid>
+                        ))}
+
                     </Grid>
                     </div>
                 </Grid>
                 <Grid item xs={2} sm={8}>
                 <Grid container  display="flex">
                 {cardData.filter(card=> (
-                        !activeYear? card.launch_year: card.launch_year == activeYear
-                        )).
-                map(crd=>(
-                    <Grid key={crd.flight_number} item xs={12} sm={4} md={4} lg={3} className={classes.card}>
-                       <Card  className={classes.cardContainer}>
-                            <CardActionArea className={classes.cardAction}>
-                                <CardMedia
-                                    component="img"
-                                    alt={crd.rocket.rocket_name}
-                                    image = {crd.links.mission_patch}
-                                    title="space"
-                                    className={classes.cardImg}
-                                />
-                            </CardActionArea>
-                            <CardContent>
-                                
-                                <h2 className={classes.cardTitle} style={{color:'blue'}}>{crd.mission_name} #{crd.flight_number}</h2>
-                                <h3>Launch Year: {crd.launch_year}</h3>
-                                <h3>Launch Success: {(crd.launch_success).toString()}</h3>
-                            </CardContent>
-                        </Card>
-                   </Grid>
-                ))}
-            </Grid>
+                        (!activeYear? card.launch_year: card.launch_year == activeYear) && (launchSuccess==null? String(card.launch_success): String(card.launch_success) == launchSuccess)
+                        )).map(crd=>(
+                            <Grid key={crd.flight_number} item xs={12} sm={4} md={4} lg={3} className={classes.card}>
+                            <Card  className={classes.cardContainer}>
+                                    <CardActionArea className={classes.cardAction}>
+                                        <CardMedia
+                                            component="img"
+                                            alt={crd.rocket.rocket_name}
+                                            image = {crd.links.mission_patch}
+                                            title="space"
+                                            className={classes.cardImg}
+                                        />
+                                    </CardActionArea>
+                                    <CardContent>
+                                        
+                                        <h2 className={classes.cardTitle} style={{color:'blue'}}>{crd.mission_name} #{crd.flight_number}</h2>
+                                        <h3>Launch Year: {crd.launch_year}</h3>
+                                        <h3>Launch Success: {(crd.launch_success).toString()}</h3>
+                                    </CardContent>
+                                </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             </Grid>
         </Grid>
             ):(<div>
